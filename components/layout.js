3e4402bddfe04e3a74027f8ee490eac35ed29ce0
Vincent Nguyen <16nguyenv@gmail.com>
2021-12-30 23:04:48 -0500

    Update site title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Vincent Nguyen'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = `${name} - Software engineer`
 
 export default function Layout({ children, home }) {
   return (
